7cb12c108213218609a7de3f72890361beff72f8
Deepak <iapain@gmail.com>
2014-09-10 15:19:56 +0200

    api changes

--- resource.js
+++ resource.js
@@ -314,9 +314,9 @@ var Resource = module.exports = Class.extend({
      */
     unauthorized:function (res, message) {
         if (message)
-            res.send(message, 401);
+            res.status(401).json(message);
         else
-            res.send(401);
+            res.status(401).end();
     },
 
     /**
@@ -326,7 +326,7 @@ var Resource = module.exports = Class.extend({
      * @param json
      */
     bad_request:function (res, json) {
-        res.json(json.message || json, 400);
+        res.status(400).json(json.message || json);
     },
 
     /**
@@ -484,14 +484,14 @@ var Resource = module.exports = Class.extend({
         res.header('Cache-Control','no-cache');
         res.header('Pragma','no-cache');
         res.header('Expires','-1');
-        res.jsonp(object, status);
+        res.status(status).jsonp(object);
     },
 
     deserializeJson : function(req,res,object,status) {
         res.header('Cache-Control','no-cache');
         res.header('Pragma','no-cache');
         res.header('Expires','-1');
-        res.json(object, status);
+        res.status(status).json(object);
     },
 
 
@@ -551,7 +551,7 @@ var Resource = module.exports = Class.extend({
                 self.internal_error(err, req, res);
             else {
                 if (!is_auth) {
-                    self.unauthorized(res,'not authenticated');
+                    self.unauthorized(res, {'error':'not authenticated'});
                     return;
                 }
 
@@ -590,18 +590,23 @@ var Resource = module.exports = Class.extend({
                                         self.unauthorized(res, err.message);
                                     }
                                     else if (err.message && err.message.match(/duplicate key/gi)) {
-                                        res.json(err.message, 400);
+                                        res.status(400).json(err.message);
                                     }
                                     else {
-                                        res.json(err.message, err.code);
+                                        res.status(err.code).json(err.message);
                                     }
                                 }
                                 else {
                                     // mongoose errors usually
-                                    if (err.errors)
+                                    if (err.errors) {
                                         self.bad_request(res, err.errors);
-									else
-										self.internal_error(err, req, res);
+                                    }
+                                    else if (err.name && err.name === "CastError") {
+                                        self.bad_request(res, err);
+                                    }
+                                    else {
+                                        self.internal_error(err, req, res);
+                                    }
                                 }
 
                                 return;
